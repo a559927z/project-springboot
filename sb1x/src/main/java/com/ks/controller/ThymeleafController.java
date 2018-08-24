83dc41354c900b7bbe6ba89c7a63a088c123e2fd
jxzhang <a559927z@163.com>
2018-08-24 18:10:13 +0800

    [*]thymeleaf

---
+++ sb1x/src/main/java/com/ks/controller/ThymeleafController.java
@@ -0,0 +1,37 @@
+package com.ks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Title: ${type_name} <br/>
+ * <p>
+ * Description: <br/>
+ *
+ * @author jxzhang
+ * @DATE 2018年08月24日 17:06
+ * @Verdion 1.0 版本
+ * ${tags}
+ */
+@Controller
+public class ThymeleafController {
+
+
+    @RequestMapping("/hi")
+    public String hello(Locale locale, Model model) {
+        model.addAttribute("greeting", "Hello!");
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("currentTime", formattedDate);
+
+        return "hello";
+    }
+}
+
